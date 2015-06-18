898835e10bce26d3c913f2fd2e47f79cf3135cd0
eslam <eslam.husseiny@gmail.com>
2015-06-18 14:32:30 +0100

    fix bunch of puppet-lint issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
+# Install oracle jdk and make it default one.
 class java (
-  $source_url = "https://download.oracle.com/otn-pub/java/jdk/7u79-b15",
+  $source_url = 'https://download.oracle.com/otn-pub/java/jdk/7u79-b15',
   $java_major_version = 7,
   $java_minor_version = 79,
   ) {
@@ -10,20 +11,20 @@ class java (
   include ::wget
   
   wget::fetch { 'jdk':
-    source              => "${source_url}/$java_filename",
-    no_cookies          => true,
-    headers             => ['Cookie: oraclelicense=accept-securebackup-cookie;'],
-    destination         => "/usr/local/$java_filename",
-    timeout             => 0,
-    nocheckcertificate  => true,
-    verbose             => false,
+    source             => "${source_url}/${java_filename}",
+    no_cookies         => true,
+    headers            => ['Cookie: oraclelicense=accept-securebackup-cookie;'],
+    destination        => "/usr/local/${java_filename}",
+    timeout            => 0,
+    nocheckcertificate => true,
+    verbose            => false,
   }
 
   package {'java':
-    provider => rpm,
-    name     => "jdk",
     ensure   => "1.${java_major_version}.0_${java_minor_version}-fcs",
-    source   => "/usr/local/$java_filename",
+    provider => rpm,
+    name     => 'jdk',
+    source   => "/usr/local/${java_filename}",
     require  => Wget::Fetch['jdk'],
   }
   ->
@@ -31,12 +32,12 @@ class java (
     ensure  => file,
     owner   => root,
     group   => root,
-    mode    => 644,
-    content => "export JAVA_HOME=/usr/java/default",
+    mode    => '0644',
+    content => 'export JAVA_HOME=/usr/java/default',
   }
   ->
   exec { 'update-java-alternatives':
-    path    => "/bin:/usr/bin:/usr/sbin",
+    path    => '/bin:/usr/bin:/usr/sbin',
     command => "alternatives --install /usr/bin/java java /usr/java/jdk1.${java_major_version}.0_${java_minor_version}/bin/java 200000",
     unless  => "[ `ls -l /etc/alternatives/java | awk '{print \$11}'` == '/usr/java/jdk1.${java_major_version}.0_${java_minor_version}/bin/java' ]"
   }
